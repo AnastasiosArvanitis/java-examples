b33b999bdac894b5749dec8b51893cdefba2f53d
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-11-15 12:37:26 +0100

    adding function testRemplir that is comparing the table before and after filling it

--- src/info/anastasiosarvanitis/java_examples/Tableaudev.java
+++ src/info/anastasiosarvanitis/java_examples/Tableaudev.java
@@ -62,28 +62,49 @@ public class Tableaudev {
 		for (int i = 0; i < arr.length; i++) {
 			ajouterElement(arr, element);
 		}
-		afficheTableau(arr);
+//		afficheTableau(arr);
 		return arr;
 	}
 	
-	public static boolean testRemplir() {
-		return false;
+	public static boolean testRemplir(int[] arrOriginal, int[] arrRempli) {
+		
+		boolean isTheSame = false;
+		
+		for (int i = 0; i < arrOriginal.length; i++) {
+			
+			for (int j = 0; j < arrRempli.length; j++) {
+				
+				if (arrOriginal[i] != arrRempli[j]) {
+					isTheSame = false;
+				} else {
+					isTheSame = true;
+				}
+			}
+		}
+		afficheTableau(arrOriginal);
+		afficheTableau(arrRempli);
+		return isTheSame;
+//		
+//		
 		
 	}
 
 	public static void main(String[] args) {
 		
-		int[] array1 = {1,5,12,58,96,78,10,21,34,89,78};
-		int[] array2 = new int[4];
+//		int[] array1 = {1,5,12,58,96,78,10,21,34,89,78};
+//		int[] array2 = new int[4];
+		
 		int[] array3 = {1,0,12,0};
-		System.out.println("array1");
-		remplir(array1, 100);
-		System.out.println();
-		System.out.println("array2");
-		remplir(array2, 100);
-		System.out.println();
-		System.out.println("array3");
-		remplir(array3, 80);
+//		System.out.println("array1");
+//		remplir(array1, 100);
+//		System.out.println();
+//		System.out.println("array2");
+//		remplir(array2, 100);
+//		System.out.println();
+//		System.out.println("array3");
+//		remplir(array3, 80);
+//		System.out.println();
+		System.out.println(testRemplir(array3, remplir(array3, 2)));
 		
 //		afficheTableau3(array3);
 	}
