ae8fa6bf2b80c2d6251caeb398391d7ab8e4667a
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-11-24 10:19:55 +0100

    string ends with

--- src/info/anastasiosarvanitis/java_examples/StringEnds.java
+++ src/info/anastasiosarvanitis/java_examples/StringEnds.java
@@ -4,15 +4,19 @@ public class StringEnds {
 
 	public static void main(String[] args) {
 		
-		System.out.println(checkEnding("Tasis", "s"));
+		System.out.println(checkEnding("Tasis", "is"));
 	}
 	
 	public static boolean checkEnding(String str1, String str2) {
-		if (str1.endsWith(str2)) {
-			return true;
-		} else {
-			return false;
-		}
+		return (str1.endsWith(str2)) ? true: false;
 	}
+	
+//	public static boolean checkEnding(String str1, String str2) {
+//		if (str1.endsWith(str2)) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
 
 }
